f3aaf00d33365c6922073bf699d8ca1cda91da70
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-07 15:03:09 -0400

    Comment

--- src/app/events/event-details/session-list.component.isolated.spec.ts
+++ src/app/events/event-details/session-list.component.isolated.spec.ts
@@ -8,6 +8,10 @@ describe('SessionListComponent', () => {
 
   beforeEach(() => {
     // First we create the component that we want to test
+    // This component's constructor has dependencies, so we provide mock versions of it
+    // Note: There is no need to configure the mock BECAUSE the SUT (the ngOnChanges() method of the component)
+    //       does not make any calls on these dependencies. If it did, we would need to do something like
+    //       jasmine.spyOnObj() ....  etc
     component = new SessionListComponent(mockAuthService, mockVoterService);
   })
 
@@ -15,6 +19,8 @@ describe('SessionListComponent', () => {
   describe('ngOnChanges', () => {
 
     it('should filter the session correctly', () => {
+      // Now we set the state variables for this component
+      // 
       component.sessions = <ISession[]>[
                                           {name: 'session 1', level: 'intermediate'},
                                           {name: 'session 2', level: 'intermediate'},
@@ -23,22 +29,30 @@ describe('SessionListComponent', () => {
       component.sortBy = 'name';
       component.eventId = 3;
 
-      // Here we want to test the ngOnChanges() method of the component.
+      // SUT --- Here we want to test the ngOnChanges() method of the component.
       component.ngOnChanges();
 
+      // Now we check expectations
       expect(component.visibleSessions.length).toBe(2);
     })
 
+    
+    // ANOTHER TEST
     it('should sort the session correctly', () => {
-      component.sessions = <ISession[]>[{name: 'session 1', level: 'intermediate'},
-        {name: 'session 3', level: 'intermediate'},
-        {name: 'session 2', level: 'beginner'}];
+      // Now we set the state variables for this component
+      // 
+      component.sessions = <ISession[]>[
+                                        {name: 'session 1', level: 'intermediate'},
+                                        {name: 'session 3', level: 'intermediate'},
+                                        {name: 'session 2', level: 'beginner'}];
       component.filterBy = 'all';
       component.sortBy = 'name';
       component.eventId = 3;
 
+      // SUT --- Here we want to test the ngOnChanges() method of the component.
       component.ngOnChanges();
 
+      // Now we check expectations
       expect(component.visibleSessions[2].name).toBe('session 3');
     })
   })
