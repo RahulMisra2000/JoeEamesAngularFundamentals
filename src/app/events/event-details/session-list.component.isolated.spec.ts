ff2c7214d7f133fba6fd42ef8884f53edff2388e
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-07 12:27:18 -0400

    Comment

--- src/app/events/event-details/session-list.component.isolated.spec.ts
+++ src/app/events/event-details/session-list.component.isolated.spec.ts
@@ -3,22 +3,27 @@ import { ISession } from '../shared/event.model'
 
 describe('SessionListComponent', () => {
   let component: SessionListComponent;
-  let mockAuthService, mockVoterService;
+  let mockAuthService, 
+      mockVoterService;
 
   beforeEach(() => {
+    // First we create the component that we want to test
     component = new SessionListComponent(mockAuthService, mockVoterService);
   })
 
+  // Note we can nest the describe()
   describe('ngOnChanges', () => {
 
     it('should filter the session correctly', () => {
-      component.sessions = <ISession[]>[{name: 'session 1', level: 'intermediate'},
-        {name: 'session 2', level: 'intermediate'},
-        {name: 'session 3', level: 'beginner'}];
+      component.sessions = <ISession[]>[
+                                          {name: 'session 1', level: 'intermediate'},
+                                          {name: 'session 2', level: 'intermediate'},
+                                          {name: 'session 3', level: 'beginner'}];
       component.filterBy = 'intermediate';
       component.sortBy = 'name';
       component.eventId = 3;
 
+      // Here we want to test the ngOnChanges() method of the component.
       component.ngOnChanges();
 
       expect(component.visibleSessions.length).toBe(2);
